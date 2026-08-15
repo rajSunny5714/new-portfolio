554ee00c9bfa032b60f1c7039a1d2d6852ebb37c
Sunny Raj <153062846+rajSunny5714@users.noreply.github.com>
2026-08-15 13:24:30 +0530

    Revise Marquee component text content
    Updated job titles and skills in the Marquee component for clarity and relevance.

--- src/components/ui/Marquee.tsx
+++ src/components/ui/Marquee.tsx
@@ -5,33 +5,32 @@ const Marquee = () => {
     <div className="bg-custom-yellow border-y-4 border-black py-3 overflow-hidden whitespace-nowrap relative rotate-[-2deg] scale-105 z-20 my-10">
       <div className="animate-marquee inline-block font-shrikhand text-2xl">
         <span className="mx-4">OPEN TO OPPORTUNITIES 🚀</span>
-        <span className="mx-4">FULL-STACK ENGINEER 💻</span>
+        <span className="mx-4">FULL-STACK Developer 💻</span>
         <span className="mx-4">ASPIRING IAM ENGINEER 💻</span>
-        <span className="mx-4">Java • Springboot • MySql/PgSql • React.js ⚡</span>
+        <span className="mx-4">Java • Springboot • Security • MySql/PgSql • React.js ⚡</span>
         <span className="mx-4">BUILD and SECURE REAL-WORLD PRODUCTS 🛠️</span>
         <span className="mx-4">AVAILABLE FOR NEW ROLES 💼</span>
 
         <span className="mx-4">OPEN TO OPPORTUNITIES 🚀</span>
-        <span className="mx-4">FULL-STACK ENGINEER 💻</span>
+        <span className="mx-4">FULL-STACK Developer 💻</span>
         <span className="mx-4">ASPIRING IAM ENGINEER 💻</span>
-        <span className="mx-4">Java • Springboot • MySql/PgSql • React.js ⚡</span>
+        <span className="mx-4">Java • Springboot • Security • MySql/PgSql • React.js ⚡</span>
         <span className="mx-4">BUILD and SECURE REAL-WORLD PRODUCTS 🛠️</span>
         <span className="mx-4">AVAILABLE FOR NEW ROLES 💼</span>
       </div>
 
-      
       <div className="animate-marquee inline-block font-shrikhand text-2xl absolute top-3 left-0">
         <span className="mx-4">OPEN TO OPPORTUNITIES 🚀</span>
-        <span className="mx-4">FULL-STACK ENGINEER 💻</span>
+        <span className="mx-4">FULL-STACK Developer 💻</span>
         <span className="mx-4">ASPIRING IAM ENGINEER 💻</span>
-        <span className="mx-4">Java • Springboot • MySql/PgSql • React.js ⚡</span>
+        <span className="mx-4">Java • Springboot • Security • MySql/PgSql • React.js ⚡</span>
         <span className="mx-4">BUILD and SECURE REAL-WORLD PRODUCTS 🛠️</span>
         <span className="mx-4">AVAILABLE FOR NEW ROLES 💼</span>
 
         <span className="mx-4">OPEN TO OPPORTUNITIES 🚀</span>
-        <span className="mx-4">FULL-STACK ENGINEER 💻</span>
+        <span className="mx-4">FULL-STACK Developer 💻</span>
         <span className="mx-4">ASPIRING IAM ENGINEER 💻</span>
-        <span className="mx-4">Java • Springboot • MySql/PgSql • React.js ⚡</span>
+        <span className="mx-4">Java • Springboot • Security • MySql/PgSql • React.js ⚡</span>
         <span className="mx-4">BUILD and SECURE REAL-WORLD PRODUCTS 🛠️</span>
         <span className="mx-4">AVAILABLE FOR NEW ROLES 💼</span>
       </div>
